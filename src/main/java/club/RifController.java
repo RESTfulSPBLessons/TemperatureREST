aaeea23d09b5b4be617629edcf66677073984a49
Anton Romanov <antonromanov@list.ru>
2019-04-05 18:29:52 +0300

    Закаментил мелочевку

--- src/main/java/club/RifController.java
+++ src/main/java/club/RifController.java
@@ -3,10 +3,7 @@ package club;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
@@ -25,17 +22,14 @@ public class RifController {
 	private final Map<String, String> modelData = new HashMap<>();
 
 
-	//private Bot bot;
-
 	/**
 	 * Логгеры
 	 */
 	private static Logger logger = LoggerFactory.getLogger(RifController.class);
 	private static Logger consoleLogger = LoggerFactory.getLogger("console_logger");
 
-	//@Autowired
+
 	public RifController() {
-		//this.bot = bot;
 	}
 
 	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
@@ -50,8 +44,6 @@ public class RifController {
 	//	errorPushToBot("Похоже отвалился сайт ИФНС: ");
 		return "welcome";
 	}
-
-
 
 
 	private void errorPushToBot(String message) {
